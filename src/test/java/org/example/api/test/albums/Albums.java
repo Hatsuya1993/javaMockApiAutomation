3f5b5a9a16d6046e8dc1fd197946bbc2725ffca1
Hazrul <hal1993@hotmail.com>
2023-10-18 20:31:45 +0800

    Complete testAlbuimsUserId

--- src/test/java/org/example/api/test/albums/Albums.java
+++ src/test/java/org/example/api/test/albums/Albums.java
@@ -22,8 +22,30 @@ public class Albums {
 
         for (int i = 0; i < jp.getInt("albums.size()"); i++) {
             assertThat("userId is invalid", jp.getInt("userId[" + i + "]") > 0);
+            assertThat("id is invalid", jp.getInt("id[" + i + "]") > 0);
+            assertThat("title is invalid",
+                    jp.getString("title[" + i + "]") != "");
         }
 
+    }
+
+    @Test(description = "Should be able to get all userId = 1")
+    private void testGetAlbumsUserId() {
+        Response res =
+                AlbumRequestHelper.getAlbumsRequestHelper(Route.postsRoute,
+                        "/albums?userId=1", 200);
+
+        assertThat("Albums of userId = 1 is empty",
+                res.asString().length() > 0);
+
+        JsonPath jp = res.jsonPath();
+        for (int i = 0; i < jp.getInt("albums.size()"); i++) {
+            assertThat("userId is incorrect",
+                    jp.getInt("userId[" + i + "]") > 0);
+            assertThat("id is incorrect", jp.getInt("id[" + i + "]") > 0);
+            assertThat("title is incorrect",
+                    jp.getString("title[" + i + "]") != "");
+        }
     }
 
 }
